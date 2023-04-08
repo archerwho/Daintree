b89a0655df016417ea48160d65f6b921c51aa76c
shubham <guptashubham9894@gmail.com>
2023-04-08 15:29:11 +0530

    Test-8

--- backend/server.js
+++ backend/server.js
@@ -7,14 +7,11 @@ const connectDB = require("./database/database");
 process.on(`uncaughtException`, (error) => {
   console.log(`Error: ${error.message}`);
   console.log(`Shutting Down the Server due to Uncaught Exception.`);
-  server.close(() => process.exit(1));
+  // server.close(() => process.exit(1));
 });
 
 //configuring the DOTENV
 dotenv.config({ path: `backend/config/.env` });
-
-//Connecting to the DB
-connectDB().catch((error) => console.log(error));
 
 //Configuration For Cloudinary
 cloudinary.config({
@@ -23,15 +20,20 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const server = app.listen(process.env.PORT, () =>
-  console.log(`Server started at http://localhost:${process.env.PORT}`)
-);
+//Connecting to the DB and listening to the PORT
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT, () =>
+      console.log(`Server started at http://localhost:${process.env.PORT}`)
+    );
+  })
+  .catch((error) => console.log(error));
 
 //Unhandled Promise Rejection
 process.on(`unhandledRejection`, (error) => {
   console.log(`Error: ${error.message}`);
   console.log(`Shutting Down the Server due to Unhandled Promise Rejection.`);
-  server.close(() => {
-    process.exit(1);
-  });
+  // server.close(() => {
+  //   process.exit(1);
+  // });
 });
